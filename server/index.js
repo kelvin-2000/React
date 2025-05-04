a64877f7f9da46cf4020f71b26da744ac5febced
Kelvin <kyawminhtet1792000@gmail.com>
2025-05-04 14:42:58 +0100

    test

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 
-//gonna create an app thru that express variable
+//gonna create an app thru that express variable  //test
 const app = express();
 
 const mysql = require('mysql2');
